2e5a06b5b2196d5efd4e1c6a1c584b89bae35e10
Alessio Manai <alessio@manai.tk>
2017-09-07 00:07:33 +0200

    correzione codice

--- TransferLearningDetail.m
+++ TransferLearningDetail.m
@@ -1,16 +1,6 @@
 
-% Deep Learning: Transfer Learning in 10 Lines of MATLAB Code 
-
-% Load Training Images
 imds = imageDatastore('pezzinew', 'IncludeSubfolders', true,...
     'LabelSource', 'foldernames');
-
-%tbl = countEachLabel(imds);
-
-%minSetCount = min(tbl{:,2}); % determine the smallest amount of images in a category
-
-% Use splitEachLabel method to trim the set.
-%imds = splitEachLabel(imds, minSetCount, 'randomize');
 
 % Split data into training and test sets 
 [trainingImages, testImages] = splitEachLabel(imds, 0.8, 'randomize');
@@ -26,19 +16,11 @@ layers(23) = fullyConnectedLayer(9); % change this based on # of classes
 layers(25) = classificationLayer
 
 % Perform Transfer Learning
-% For transfer learning we want to change the weights of the network ever so slightly. How
-% much a network is changed during training is controlled by the learning
-% rates. 
+% For transfer learning we want to change the weights of the network ever so slightly.
 opts = trainingOptions('sgdm', 'InitialLearnRate', 0.001,...
     'MaxEpochs', 20, 'MiniBatchSize', 64);
 
 % Set custom read function 
-% One of the great things about imageDataStore it lets you specify a
-% "custom" read function, in this case it is simply resizing the input
-% images to 227x227 pixels which is what AlexNet expects. You can do this by
-% specifying a function handle of a function with code to read and
-% pre-process the image. 
-
 trainingImages.ReadFcn = @readFunctionTrain;
 
 % Train the Network 
@@ -47,7 +29,6 @@ myNet = trainNetwork(trainingImages, layers, opts);
 
 
 % Test Network Performance
-% Now let's the test the performance of our new "snack recognizer" on the test set.
 testImages.ReadFcn = @readFunctionTrain;
 predictedLabels = classify(myNet, testImages); 
 accuracy = mean(predictedLabels == testImages.Labels)
