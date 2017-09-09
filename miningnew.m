38c9b6686f90705197606dca8f81d1daf5ff8c50
Alessio Manai <alessio@manai.tk>
2017-09-09 18:09:00 +0200

    aggiunte label miningnew.m

--- miningnew.m
+++ miningnew.m
@@ -11,53 +11,62 @@ confMatrix = evaluate(categoryClassifier, trainingSet);
 
 confMatrix = evaluate(categoryClassifier, testSet);
 
-
+fprintf('regina nera');
 img = imread('detectionnew/1.jpg'); %regina nera
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('cavallo bianco');
 img = imread('detectionnew/2.jpg'); %cavallo bianco 
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('pedone bianco');
 img = imread('detectionnew/3.jpg'); %pedone bianco
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('cavallo nero');
 img = imread('detectionnew/4.jpg'); %cavallo nero
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('regina bianca');
 img = imread('detectionnew/5.jpg'); %regina bianca
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('cavallo bianco');
 img = imread('detectionnew/6.jpg'); %cavallo bianco
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('regina nera');
 img = imread('detectionnew/7.jpg'); %regina nera
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('regina bianca')
 img = imread('detectionnew/8.jpg'); %regina bianca
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
+fprintf('cavallo nero');
 img = imread('detectionnew/9.jpg'); %cavallo nero
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
 
-img = imread('detectionnew/10.jpg'); %pedone bianco
+fprintf('pedone bianco');
+img = imread('detectionnew/14.jpg'); %pedone bianco
 [labelIdx, scores] = predict(categoryClassifier, img);
 % Display the string label
 categoryClassifier.Labels(labelIdx)
